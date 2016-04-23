1eb732800ebeed1e1319ab79431ba05a749d9c2d
a.bazhanau <alex.bazhanau@adform.com>
2016-04-23 21:21:19 +0300

    couchbase service implemented

--- OsmTest.Core/Services/CouchDbGeoObjectsService.cs
+++ OsmTest.Core/Services/CouchDbGeoObjectsService.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using Couchbase.Lite;
 using GeoJSON.Net.Feature;
 
 namespace OsmTest.Core.Services
 {
     public class CouchDbGeoObjectsService : IGeoObjectsService
     {
+        private readonly string _viewName;
+        private readonly Database _db = Manager.SharedInstance.GetDatabase("main");
+
+        public CouchDbGeoObjectsService(Uri serverUrl, string viewName)
+        {
+            _viewName = viewName;
+
+            //var replication = _db.CreatePullReplication(serverUrl);
+            //replication.Continuous = true;
+            //replication.Start();
+        }
+
         public FeatureCollection GetCloseUsers(Feature point, double radius)
         {
-            
+            var res = new FeatureCollection();
+            using (var query = _db.GetView(_viewName).CreateQuery())
+            {
+                foreach (var entry in query.Run())
+                {
+                    res.Features.Add(entry.ValueAs<Feature>());
+                }
+            }
+
+            return res;
         }
     }
 }
