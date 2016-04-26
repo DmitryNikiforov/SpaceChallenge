11fb739015b946aca44e3f9a83183ea1c5b33348
a.bazhanau <alex.bazhanau@adform.com>
2016-04-26 11:46:03 +0300

    server project merged

---
+++ SpaceHerdersServer/Services/InMemoryUsersLocationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Space_Herdsmans.Models;
+
+namespace Space_Herdsmans.Services
+{
+    public class InMemoryUsersLocationService : IUsersLocationService
+    {
+        private readonly IDictionary<Guid, GeoCoordinates> _data = new ConcurrentDictionary<Guid, GeoCoordinates>();
+ 
+        public ICollection<GeoCoordinates> GetCloseUsers(GeoCoordinates point, double radius)
+        {
+            return _data.Values;
+        }
+
+        public GeoCoordinates GetLastUserPosition(Guid userId)
+        {
+            GeoCoordinates coordinates;
+            _data.TryGetValue(userId, out coordinates);
+
+            return coordinates;
+        }
+
+        public void UpdateUserPosition(Guid userId, GeoCoordinates point)
+        {
+            _data[userId] = point;
+        }
+    }
+}
